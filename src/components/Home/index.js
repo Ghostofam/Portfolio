26092a5313ff2b942717e642c9034f7ce1f788c5
Ghostofam <aqeelabbas84@gmail.com>
2023-09-01 11:27:36 +0500

    Routes updated

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -40,7 +40,7 @@ const Home=()=>{
                 <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={15}/>
                 </h1>
                 <h2>Frontend Developer / Data Scientist / Data Analyst</h2>
-                <Link to="/contact" className="flat-button">CONTACT ME</Link>
+                <Link to="/Portfolio/contact" className="flat-button">CONTACT ME</Link>
             </div>
             <Logo />
         </div>
